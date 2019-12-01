239e06b06558830580d5d7182d73fc40d05e842f
Roger Lipscombe <roger@differentpla.net>
2019-12-01 20:06:38 +0000

    1.2: Solution

--- day1a/lib/day1a.ex
+++ day1a/lib/day1a.ex
@@ -1,20 +1,33 @@
 defmodule Day1a do
   @moduledoc """
-  Implementation of Day, Puzzle 1 of Advent of Code, 2019.
+  Implementation of Day 1 of Advent of Code, 2019.
   """
 
   def from_file(path) do
-    File.read!(path)
-    |> String.split("\n", trim: true)
-    |> Enum.map(&String.to_integer/1)
+    modules =
+      File.read!(path)
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    modules
     |> Enum.map(&get_fuel_for_module/1)
-    |> Enum.sum
+    |> Enum.sum()
   end
 
   @doc """
   For a given module, work out how much fuel we need.
   """
   def get_fuel_for_module(mass) do
-    div(mass, 3) - 2
+    # Starting with the mass of the module, get the fuel.
+    fuel = get_fuel_for_mass(mass)
+
+    # Iterate, getting the fuel for the fuel,
+    # until we get to zero.
+    # Sum.
+    Stream.iterate(fuel, fn m -> get_fuel_for_mass(m) end)
+    |> Stream.take_while(fn m -> m > 0 end)
+    |> Enum.sum()
   end
+
+  def get_fuel_for_mass(mass), do: div(mass, 3) - 2
 end
--- day1a/test/day1a_test.exs
+++ day1a/test/day1a_test.exs
@@ -2,10 +2,20 @@ defmodule Day1aTest do
   use ExUnit.Case
   doctest Day1a
 
-  test "basic numbers" do
-    assert Day1a.get_fuel_for_module(12) == 2
+  test "fuel for mass" do
+    assert Day1a.get_fuel_for_mass(12) == 2
+    assert Day1a.get_fuel_for_mass(14) == 2
+    assert Day1a.get_fuel_for_mass(1969) == 654
+    assert Day1a.get_fuel_for_mass(100756) == 33583
+  end
+
+  test "fuel for module" do
     assert Day1a.get_fuel_for_module(14) == 2
-    assert Day1a.get_fuel_for_module(1969) == 654
-    assert Day1a.get_fuel_for_module(100756) == 33583
+    assert Day1a.get_fuel_for_module(1969) == 966
+    assert Day1a.get_fuel_for_module(100756) == 50346
+  end
+
+  test "final result" do
+    Day1a.from_file("test-data.txt") |> IO.inspect
   end
 end
