7cdac051cbe4c5be83a0262e4f7b9d3743601c01
Roger Lipscombe <roger@differentpla.net>
2019-12-01 17:05:37 +0000

    1.1: Initial creation

---
+++ day1a/lib/day1a.ex
@@ -0,0 +1,18 @@
+defmodule Day1a do
+  @moduledoc """
+  Documentation for Day1a.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day1a.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ day1a/test/day1a_test.exs
@@ -0,0 +1,8 @@
+defmodule Day1aTest do
+  use ExUnit.Case
+  doctest Day1a
+
+  test "greets the world" do
+    assert Day1a.hello() == :world
+  end
+end
