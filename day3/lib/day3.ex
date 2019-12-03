c2f652d245be1c4a02fbd61db06e000d666f0810
Roger Lipscombe <roger@differentpla.net>
2019-12-03 19:57:28 +0000

    sdhsdhsd

---
+++ day3/lib/day3.ex
@@ -0,0 +1,72 @@
+defmodule Day3 do
+  def paths_from_file(f) do
+    [a,b] = File.read!(f) |> String.split("\n", trim: true)
+    {a, b}
+  end
+
+  def path_from_string(s) do
+    String.split(s, ",") |> Enum.map(&to_step/1)
+  end
+
+  defp to_step("R" <> n), do: {:r, String.to_integer(n)}
+  defp to_step("D" <> n), do: {:d, String.to_integer(n)}
+  defp to_step("U" <> n), do: {:u, String.to_integer(n)}
+  defp to_step("L" <> n), do: {:l, String.to_integer(n)}
+
+  @doc """
+  Given two paths, work out where the closest intersection to the start is.
+  """
+  def get_closest_intersection_for_paths(path_a, path_b) do
+    # Work out which points each path visits.
+    {points_a, _} = get_points_visited_by_path(path_a)
+    {points_b, _} = get_points_visited_by_path(path_b)
+
+    # Work out where they intersect.
+    intersections = get_shared_points(points_a, points_b)
+
+    # Which of those is closest to the origin?
+    intersections |> MapSet.delete({0, 0})
+    |> Enum.map(&manhatten/1)
+    |> Enum.sort
+    |> Enum.at(0)
+  end
+
+  def get_points_visited_by_path(path) do
+    get_points_visited_by_path(path_from_string(path), {0, 0}, %MapSet{})
+  end
+
+  defp get_points_visited_by_path([step | rest], pos0, visited0) do
+    {visited, pos} = do_step(step, pos0, visited0)
+    get_points_visited_by_path(rest, pos, visited)
+  end
+
+  defp get_points_visited_by_path([], pos, visited), do: {visited, pos}
+
+  defp do_step({:l, n}, {x, y}, visited) do
+    visited = Enum.reduce(x..(x - n), visited, fn i, v -> MapSet.put(v, {i, y}) end)
+    {visited, {x - n, y}}
+  end
+
+  defp do_step({:r, n}, {x, y}, visited) do
+    visited = Enum.reduce(x..(x + n), visited, fn i, v -> MapSet.put(v, {i, y}) end)
+    {visited, {x + n, y}}
+  end
+
+  defp do_step({:u, n}, {x, y}, visited) do
+    visited = Enum.reduce(y..(y + n), visited, fn j, v -> MapSet.put(v, {x, j}) end)
+    {visited, {x, y + n}}
+  end
+
+  defp do_step({:d, n}, {x, y}, visited) do
+    visited = Enum.reduce(y..(y - n), visited, fn j, v -> MapSet.put(v, {x, j}) end)
+    {visited, {x, y - n}}
+  end
+
+  defp get_shared_points(a, b) do
+    MapSet.intersection(a, b)
+  end
+
+  defp manhatten({x, y}) do
+    abs(x) + abs(y)
+  end
+end
---
+++ day3/test/day3_test.exs
@@ -0,0 +1,54 @@
+defmodule Day3Test do
+  use ExUnit.Case
+  doctest Day3
+
+  test "path from string" do
+    assert Day3.path_from_string("R75,D30,R83,U83,L12") == [
+             {:r, 75},
+             {:d, 30},
+             {:r, 83},
+             {:u, 83},
+             {:l, 12}
+           ]
+  end
+
+  test "get_points_visited_by_path 1" do
+    assert {_, {8,0}} = Day3.get_points_visited_by_path("R8")
+  end
+
+  test "get_points_visited_by_path 2" do
+    assert {_, {8,5}} = Day3.get_points_visited_by_path("R8,U5")
+  end
+
+  test "get_points_visited_by_path 3" do
+    assert {_, {3,5}} = Day3.get_points_visited_by_path("R8,U5,L5")
+  end
+
+  test "get_points_visited_by_path 4" do
+    assert {_, {3,2}} = Day3.get_points_visited_by_path("R8,U5,L5,D3")
+  end
+
+  test "negative points" do
+    assert {_, {-3, -6}} = Day3.get_points_visited_by_path("R1,U1,L4,D7")
+    assert Day3.get_closest_intersection_for_paths("R1,U1", "U1,R1") == 2
+  end
+
+  test "first example" do
+    assert Day3.get_closest_intersection_for_paths(
+             "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+             "U62,R66,U55,R34,D71,R55,D58,R83"
+           ) == 159
+  end
+
+  test "second example" do
+    assert Day3.get_closest_intersection_for_paths(
+             "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+             "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+           ) == 135
+  end
+
+  test "puzzle" do
+    {a, b} = Day3.paths_from_file("test-data.txt")
+    Day3.get_closest_intersection_for_paths(a, b) |> IO.inspect
+  end
+end
