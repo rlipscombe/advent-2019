3e9d193a1f1c317af30a906ea6a3a0b9d9802fd3
Roger Lipscombe <roger@differentpla.net>
2019-12-06 16:05:27 +0000

    sdhdshs

--- day6/lib/day6.ex
+++ day6/lib/day6.ex
@@ -1,31 +1,56 @@
 defmodule Day6 do
-  def main([file]) do
+  def main([verb, file]) do
     data =
       File.read!(file)
       |> String.trim()
       |> String.split("\n")
       |> Enum.map(fn s -> String.split(s, ")") end)
-      |> IO.inspect()
 
     g =
       Enum.reduce(data, Graph.new(type: :directed), fn [a, b], g ->
         g |> Graph.add_vertex(a) |> Graph.add_vertex(b) |> Graph.add_edge(a, b)
       end)
 
-    # dot -Tpng -o test-data.png test-data.dot
-    # xdg-open test-data.png
-    {:ok, dot} = Graph.to_dot(g)
-    File.write!(Path.rootname(file) <> ".dot", dot)
+    case verb do
+      "dot" ->
+        # dot -Tpng -o test-data.png test-data.dot
+        # xdg-open test-data.png
+        {:ok, dot} = Graph.to_dot(g)
+        File.write!(Path.rootname(file) <> ".dot", dot)
 
-    g = add_distances(g, "COM")
+        System.cmd("dot", [
+          "-Tpng",
+          "-o",
+          Path.rootname(file) <> ".png",
+          Path.rootname(file) <> ".dot"
+        ])
 
-    result = Graph.Reducers.Dfs.reduce(g, 0, fn v, acc ->
-      [d] = Graph.vertex_labels(g, v)
-      IO.puts("#{v} has distance #{d}")
-      {:next, acc + d}
-    end)
+      "distances" ->
+        g = add_distances(g, "COM")
 
-    IO.puts result
+        result =
+          Graph.Reducers.Dfs.reduce(g, 0, fn v, acc ->
+            [d] = Graph.vertex_labels(g, v)
+            IO.puts("#{v} has distance #{d}")
+            {:next, acc + d}
+          end)
+
+        IO.puts(result)
+
+      "transfer" ->
+        edges = Graph.edges(g)
+
+        g =
+          Enum.reduce(edges, g, fn e, g ->
+            a = e.v1
+            b = e.v2
+            Graph.add_edge(g, b, a)
+          end)
+
+        vs = Graph.get_shortest_path(g, "YOU", "SAN") |> IO.inspect()
+        result = length(vs) - 3
+        IO.puts(result)
+    end
   end
 
   defp add_distances(g, root) do
