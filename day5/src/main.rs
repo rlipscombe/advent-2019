91344cc0bc5f9b9662a36307227215a30ef3d5ad
Roger Lipscombe <roger@differentpla.net>
2019-12-06 14:33:37 +0000

    dshdsh

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -14,6 +14,22 @@ const OP_MUL_II: i32 = 11_02;
 const OP_INPUT_P: i32 = 0_03;
 const OP_OUTPUT_P: i32 = 0_04;
 const OP_OUTPUT_I: i32 = 1_04;
+const OP_JT_PP: i32 = 00_05;
+const OP_JT_IP: i32 = 01_05;
+const OP_JT_PI: i32 = 10_05;
+const OP_JT_II: i32 = 11_05;
+const OP_JF_PP: i32 = 00_06;
+const OP_JF_IP: i32 = 01_06;
+const OP_JF_PI: i32 = 10_06;
+const OP_JF_II: i32 = 11_06;
+const OP_LT_PP: i32 = 00_07;
+const OP_LT_IP: i32 = 01_07;
+const OP_LT_PI: i32 = 10_07;
+const OP_LT_II: i32 = 11_07;
+const OP_EQ_PP: i32 = 00_08;
+const OP_EQ_IP: i32 = 01_08;
+const OP_EQ_PI: i32 = 10_08;
+const OP_EQ_II: i32 = 11_08;
 const OP_HALT: i32 = 99;
 
 impl Interpreter {
@@ -24,7 +40,8 @@ impl Interpreter {
         }
     }
 
-    fn run(&mut self) -> Vec<i32> {
+    fn run(&mut self, inputs: Vec<i32>) -> Vec<i32> {
+        let mut input_cursor = 0;
         let mut outputs: Vec<i32> = Vec::new();
         loop {
             match self.read_op(self.ip) {
@@ -34,7 +51,8 @@ impl Interpreter {
                     let trg_p = self.read(self.ip + 3) as usize;
                     let result = lhs + rhs;
                     println!(
-                        "[{}] <- [{}] + [{}]  ; {} + {} = {}",
+                        "{}: [{}] <- [{}] + [{}]  ; {} + {} = {}",
+                        self.ip,
                         self.read(self.ip + 3),
                         self.read(self.ip + 1),
                         self.read(self.ip + 2),
@@ -51,7 +69,8 @@ impl Interpreter {
                     let trg_p = self.read(self.ip + 3) as usize;
                     let result = lhs + rhs;
                     println!(
-                        "[{}] <- {} + {}  ; {} + {} = {}",
+                        "{}: [{}] <- {} + {}  ; {} + {} = {}",
+                        self.ip,
                         self.read(self.ip + 3),
                         self.read(self.ip + 1),
                         self.read(self.ip + 2),
@@ -68,7 +87,8 @@ impl Interpreter {
                     let trg_p = self.read(self.ip + 3) as usize;
                     let result = lhs + rhs;
                     println!(
-                        "[{}] <- {} + [{}]  ; {} + {} = {}",
+                        "{}: [{}] <- {} + [{}]  ; {} + {} = {}",
+                        self.ip,
                         self.read(self.ip + 3),
                         self.read(self.ip + 1),
                         self.read(self.ip + 2),
@@ -85,7 +105,8 @@ impl Interpreter {
                     let trg_p = self.read(self.ip + 3) as usize;
                     let result = lhs + rhs;
                     println!(
-                        "[{}] <- [{}] + {}  ; {} + {} = {}",
+                        "{}: [{}] <- [{}] + {}  ; {} + {} = {}",
+                        self.ip,
                         self.read(self.ip + 3),
                         self.read(self.ip + 1),
                         self.read(self.ip + 2),
@@ -102,7 +123,8 @@ impl Interpreter {
                     let trg_p = self.read(self.ip + 3) as usize;
                     let result = lhs * rhs;
                     println!(
-                        "[{}] <- [{}] * [{}]  ; {} - {} = {}",
+                        "{}: [{}] <- [{}] * [{}]  ; {} - {} = {}",
+                        self.ip,
                         self.read(self.ip + 3),
                         self.read(self.ip + 1),
                         self.read(self.ip + 2),
@@ -119,7 +141,8 @@ impl Interpreter {
                     let trg_p = self.read(self.ip + 3) as usize;
                     let result = lhs * rhs;
                     println!(
-                        "[{}] <- {} * [{}]  ; {} - {} = {}",
+                        "{}: [{}] <- {} * [{}]  ; {} - {} = {}",
+                        self.ip,
                         self.read(self.ip + 3),
                         self.read(self.ip + 1),
                         self.read(self.ip + 2),
@@ -136,7 +159,8 @@ impl Interpreter {
                     let trg_p = self.read(self.ip + 3) as usize;
                     let result = lhs * rhs;
                     println!(
-                        "[{}] <- [{}] * {}  ; {} - {} = {}",
+                        "{}: [{}] <- [{}] * {}  ; {} - {} = {}",
+                        self.ip,
                         self.read(self.ip + 3),
                         self.read(self.ip + 1),
                         self.read(self.ip + 2),
@@ -153,7 +177,8 @@ impl Interpreter {
                     let trg_p = self.read(self.ip + 3) as usize;
                     let result = lhs * rhs;
                     println!(
-                        "[{}] <- {} * {}  ; {} - {} = {}",
+                        "{}: [{}] <- {} * {}  ; {} - {} = {}",
+                        self.ip,
                         self.read(self.ip + 3),
                         self.read(self.ip + 1),
                         self.read(self.ip + 2),
@@ -166,25 +191,280 @@ impl Interpreter {
                 }
                 OP_INPUT_P => {
                     let p = self.read(self.ip + 1) as usize;
-                    let value = 1; // the only input
-                    println!("[{}] <- 1", self.read(self.ip + 1));
+                    let value = inputs[input_cursor];
+                    input_cursor += 1;
+                    println!(
+                        "{}: [{}] <- in  ; {}",
+                        self.ip,
+                        self.read(self.ip + 1),
+                        value
+                    );
                     self.write(p, value);
                     self.ip += 2;
                 }
                 OP_OUTPUT_P => {
                     let value = self.read_via(self.ip + 1);
-                    println!("out [{}] = {}", self.read(self.ip + 1), value);
+                    println!("{}: out [{}] = {}", self.ip, self.read(self.ip + 1), value);
                     outputs.push(value);
                     self.ip += 2;
                 }
                 OP_OUTPUT_I => {
                     let value = self.read(self.ip + 1);
-                    println!("out {} = {}", self.read(self.ip + 1), value);
+                    println!("{}: out {} = {}", self.ip, self.read(self.ip + 1), value);
                     outputs.push(value);
                     self.ip += 2;
                 }
+                OP_JT_IP => {
+                    let val = self.read(self.ip + 1);
+                    let dst = self.read_via(self.ip + 2) as usize;
+                    println!(
+                        "{}: jt [{}] {}",
+                        self.ip,
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2)
+                    );
+                    if val != 0 {
+                        self.ip = dst;
+                    } else {
+                        self.ip += 3;
+                    }
+                }
+                OP_JT_PI => {
+                    let val = self.read_via(self.ip + 1);
+                    let dst = self.read(self.ip + 2) as usize;
+                    println!(
+                        "{}: jt [{}] {}",
+                        self.ip,
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2)
+                    );
+                    if val != 0 {
+                        self.ip = dst;
+                    } else {
+                        self.ip += 3;
+                    }
+                }
+                OP_JT_II => {
+                    let val = self.read(self.ip + 1);
+                    let dst = self.read(self.ip + 2) as usize;
+                    println!(
+                        "{}: jt {} {}",
+                        self.ip,
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2)
+                    );
+                    if val != 0 {
+                        self.ip = dst;
+                    } else {
+                        self.ip += 3;
+                    }
+                }
+                OP_JF_PP => {
+                    let val = self.read_via(self.ip + 1);
+                    let dst = self.read_via(self.ip + 2) as usize;
+                    println!(
+                        "{}: jf [{}] [{}]",
+                        self.ip,
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2)
+                    );
+                    if val == 0 {
+                        self.ip = dst;
+                    } else {
+                        self.ip += 3;
+                    }
+                }
+                OP_JF_IP => {
+                    let val = self.read(self.ip + 1);
+                    let dst = self.read_via(self.ip + 2) as usize;
+                    println!(
+                        "{}: jf [{}] {}",
+                        self.ip,
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2)
+                    );
+                    if val == 0 {
+                        self.ip = dst;
+                    } else {
+                        self.ip += 3;
+                    }
+                }
+                OP_JF_PI => {
+                    let val = self.read_via(self.ip + 1);
+                    let dst = self.read(self.ip + 2) as usize;
+                    println!(
+                        "{}: jf [{}] {}",
+                        self.ip,
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2)
+                    );
+                    if val == 0 {
+                        self.ip = dst;
+                    } else {
+                        self.ip += 3;
+                    }
+                }
+                OP_JF_II => {
+                    let val = self.read(self.ip + 1);
+                    let dst = self.read(self.ip + 2) as usize;
+                    println!(
+                        "{}: jf {} {}",
+                        self.ip,
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2)
+                    );
+                    if val == 0 {
+                        self.ip = dst;
+                    } else {
+                        self.ip += 3;
+                    }
+                }
+                OP_EQ_PP => {
+                    let lhs = self.read_via(self.ip + 1);
+                    let rhs = self.read_via(self.ip + 2);
+                    let trg_p = self.read(self.ip + 3) as usize;
+                    let result = lhs == rhs;
+                    println!(
+                        "{}: [{}] <- [{}] eq [{}]  ; {} == {} = {}",
+                        self.ip,
+                        self.read(self.ip + 3),
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2),
+                        lhs,
+                        rhs,
+                        result
+                    );
+                    self.write(trg_p, result as i32);
+                    self.ip += 4;
+                }
+                OP_EQ_IP => {
+                    let lhs = self.read(self.ip + 1);
+                    let rhs = self.read_via(self.ip + 2);
+                    let trg_p = self.read(self.ip + 3) as usize;
+                    let result = lhs == rhs;
+                    println!(
+                        "{}: [{}] <- {} eq [{}]  ; {} == {} = {}",
+                        self.ip,
+                        self.read(self.ip + 3),
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2),
+                        lhs,
+                        rhs,
+                        result
+                    );
+                    self.write(trg_p, result as i32);
+                    self.ip += 4;
+                }
+                OP_EQ_PI => {
+                    let lhs = self.read_via(self.ip + 1);
+                    let rhs = self.read(self.ip + 2);
+                    let trg_p = self.read(self.ip + 3) as usize;
+                    let result = lhs == rhs;
+                    println!(
+                        "{}: [{}] <- {} eq [{}]  ; {} == {} = {}",
+                        self.ip,
+                        self.read(self.ip + 3),
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2),
+                        lhs,
+                        rhs,
+                        result
+                    );
+                    self.write(trg_p, result as i32);
+                    self.ip += 4;
+                }
+                OP_EQ_II => {
+                    let lhs = self.read(self.ip + 1);
+                    let rhs = self.read(self.ip + 2);
+                    let trg_p = self.read(self.ip + 3) as usize;
+                    let result = lhs == rhs;
+                    println!(
+                        "{}: [{}] <- {} eq {}  ; {} == {} = {}",
+                        self.ip,
+                        self.read(self.ip + 3),
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2),
+                        lhs,
+                        rhs,
+                        result
+                    );
+                    self.write(trg_p, result as i32);
+                    self.ip += 4;
+                }
+                OP_LT_PP => {
+                    let lhs = self.read_via(self.ip + 1);
+                    let rhs = self.read_via(self.ip + 2);
+                    let trg_p = self.read(self.ip + 3) as usize;
+                    let result = lhs < rhs;
+                    println!(
+                        "{}: [{}] <- [{}] lt [{}]  ; {} lt {} = {}",
+                        self.ip,
+                        self.read(self.ip + 3),
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2),
+                        lhs,
+                        rhs,
+                        result
+                    );
+                    self.write(trg_p, result as i32);
+                    self.ip += 4;
+                }
+                OP_LT_IP => {
+                    let lhs = self.read(self.ip + 1);
+                    let rhs = self.read_via(self.ip + 2);
+                    let trg_p = self.read(self.ip + 3) as usize;
+                    let result = lhs < rhs;
+                    println!(
+                        "{}: [{}] <- [{}] lt {}  ; {} lt {} = {}",
+                        self.ip,
+                        self.read(self.ip + 3),
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2),
+                        lhs,
+                        rhs,
+                        result
+                    );
+                    self.write(trg_p, result as i32);
+                    self.ip += 4;
+                }
+                OP_LT_PI => {
+                    let lhs = self.read_via(self.ip + 1);
+                    let rhs = self.read(self.ip + 2);
+                    let trg_p = self.read(self.ip + 3) as usize;
+                    let result = lhs < rhs;
+                    println!(
+                        "{}: [{}] <- [{}] lt {}  ; {} lt {} = {}",
+                        self.ip,
+                        self.read(self.ip + 3),
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2),
+                        lhs,
+                        rhs,
+                        result
+                    );
+                    self.write(trg_p, result as i32);
+                    self.ip += 4;
+                }
+                OP_LT_II => {
+                    let lhs = self.read(self.ip + 1);
+                    let rhs = self.read(self.ip + 2);
+                    let trg_p = self.read(self.ip + 3) as usize;
+                    let result = lhs < rhs;
+                    println!(
+                        "{}: [{}] <- [{}] lt {}  ; {} lt {} = {}",
+                        self.ip,
+                        self.read(self.ip + 3),
+                        self.read(self.ip + 1),
+                        self.read(self.ip + 2),
+                        lhs,
+                        rhs,
+                        result
+                    );
+                    self.write(trg_p, result as i32);
+                    self.ip += 4;
+                }
                 OP_HALT => {
-                    println!("halt");
+                    println!("{}: halt", self.ip);
                     return outputs;
                 }
                 op => {
@@ -215,7 +495,7 @@ fn main() {
     let source = std::fs::read_to_string("test-data.txt").unwrap();
     let bytes = parse(source);
     let mut computer = Interpreter::new(bytes);
-    computer.run();
+    computer.run(vec![5]);
 }
 
 // A given value could be code or data; leave this as a Vec.
@@ -231,26 +511,96 @@ fn parse(source: String) -> Vec<i32> {
 fn test_add_pp() {
     let bytes = parse("1,7,8,0,4,0,99,-12,12".to_string());
     let mut computer = Interpreter::new(bytes);
-    assert_eq!(vec![0], computer.run());
+    assert_eq!(vec![0], computer.run(vec![]));
 }
 
 #[test]
 fn test_add_ip() {
     let bytes = parse("101,-12,8,0,4,0,99,0,12".to_string());
     let mut computer = Interpreter::new(bytes);
-    assert_eq!(vec![0], computer.run());
+    assert_eq!(vec![0], computer.run(vec![]));
 }
 
 #[test]
 fn test_add_pi() {
     let bytes = parse("1001,8,-12,0,4,0,99,0,12".to_string());
     let mut computer = Interpreter::new(bytes);
-    assert_eq!(vec![0], computer.run());
+    assert_eq!(vec![0], computer.run(vec![]));
 }
 
 #[test]
 fn test_add_ii() {
     let bytes = parse("1101,123,-123,0,4,0,99".to_string());
     let mut computer = Interpreter::new(bytes);
-    assert_eq!(vec![0], computer.run());
+    assert_eq!(vec![0], computer.run(vec![]));
+}
+
+#[test]
+fn test_eq_8() {
+    let bytes = parse("3,9,8,9,10,9,4,9,99,-1,8".to_string());
+    let mut computer = Interpreter::new(bytes);
+    assert_eq!(vec![1], computer.run(vec![8]));
+}
+
+#[test]
+fn test_neq_8() {
+    let bytes = parse("3,9,8,9,10,9,4,9,99,-1,8".to_string());
+    let mut computer = Interpreter::new(bytes);
+    assert_eq!(vec![0], computer.run(vec![-8]));
+}
+
+#[test]
+fn test_lt_8() {
+    let bytes = parse("3,9,7,9,10,9,4,9,99,-1,8".to_string());
+    let mut computer = Interpreter::new(bytes);
+    assert_eq!(vec![1], computer.run(vec![7]));
+}
+
+#[test]
+fn test_nlt_8() {
+    let bytes = parse("3,9,7,9,10,9,4,9,99,-1,8".to_string());
+    let mut computer = Interpreter::new(bytes);
+    assert_eq!(vec![0], computer.run(vec![8]));
+}
+
+#[test]
+fn test_eq_ii_8() {
+    let bytes = parse("3,3,1108,-1,8,3,4,3,99".to_string());
+    let mut computer = Interpreter::new(bytes);
+    assert_eq!(vec![1], computer.run(vec![8]));
+}
+
+#[test]
+fn test_neq_ii_8() {
+    let bytes = parse("3,3,1108,-1,8,3,4,3,99".to_string());
+    let mut computer = Interpreter::new(bytes);
+    assert_eq!(vec![0], computer.run(vec![7]));
+}
+
+#[test]
+fn test_jf_pp_z() {
+    let bytes = parse("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9".to_string());
+    let mut computer = Interpreter::new(bytes);
+    assert_eq!(vec![0], computer.run(vec![0]));
+}
+
+#[test]
+fn test_jt_ii_z() {
+    let bytes = parse("3,3,1105,-1,9,1101,0,0,12,4,12,99,1".to_string());
+    let mut computer = Interpreter::new(bytes);
+    assert_eq!(vec![0], computer.run(vec![0]));
+}
+
+#[test]
+fn test_jf_pp_nz() {
+    let bytes = parse("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9".to_string());
+    let mut computer = Interpreter::new(bytes);
+    assert_eq!(vec![1], computer.run(vec![42]));
+}
+
+#[test]
+fn test_jt_ii_nz() {
+    let bytes = parse("3,3,1105,-1,9,1101,0,0,12,4,12,99,1".to_string());
+    let mut computer = Interpreter::new(bytes);
+    assert_eq!(vec![1], computer.run(vec![42]));
 }
